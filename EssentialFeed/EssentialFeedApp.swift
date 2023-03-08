635f572897899d44cd176e50e0db98ed65759533
Diego Vera <veradiego31@gmail.com>
2023-03-08 17:25:27 -0300

    Initial Commit

---
+++ EssentialFeed/EssentialFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  EssentialFeedApp.swift
+//  EssentialFeed
+//
+//  Created by Diego Alberto Vera Meaurio on 2023-03-08.
+//
+
+import SwiftUI
+
+@main
+struct EssentialFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
